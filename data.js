4cc4128b176553e2f822a7c2b1fdf45249af3258
winds <winds@wings.local>
2016-07-20 00:32:56 +0800

    add fellerTructer mobile project

---
+++ data.js
@@ -0,0 +1,20 @@
+data = {
+    "title":"FellowTrucker",
+    "header":{
+        "title":"OUR MISSON",
+        "nav":[
+            {"name":"CONNECT","href":""},
+            {"name":"SHARE","href":""},
+            {"name":"EMPROER","href":""}
+        ],
+        "p1":"Help fellow truckers,join <a href=''>felltrucker.com</a>",
+        "p2":"Share your rates,make fair rate for everyone."
+    },
+    "table":{
+        "meta":["Age","Book&nbsp;Date","Rate","P/U&nbsp;Date","Del&nbsp;Date","Origin","Destination","Trailer","Picks/Drop","Weight","F/P","Company"],
+        "data":[
+            ["00d:14h:01min","07/15","$2950.00","07/15","07/18","Gary,MN","Los Angeles,CA","R","1p/1d","20klbs","F","MW Trucking/carrier",{"Comments":"55-65 DEGREES"}],
+            ["00d:14h:01min","07/15","$2950.00","07/15","07/18","Gary,MN","Los Angeles,CA","R","1p/1d","20klbs","F","MW Trucking/carrier",{"Comments":""}]
+        ]
+    }
+};
